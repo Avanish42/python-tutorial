640e20f2e9806ee945d129fc68edbfe83455c0a3
saurabhsinghbhadoria <sourabh.bhadoria@gmail.com>
2025-09-30 14:27:30 -0400

    Delete question5.py

--- question5.py
+++
@@ -1,5 +0,0 @@
-numbers = {10, 20, 20, 30, 40, 40} #set unordered and not sequence and no duplicate
-print(numbers)
-
-numbers = [10, 20, 20, 30, 40, 40] # list ordered and sequence and duplicate
-print(numbers)
